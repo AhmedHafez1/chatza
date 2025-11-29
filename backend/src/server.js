e688902ab261422b152b1176fc617bf4332bbe6f
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2025-11-29 07:05:40 +0200

    Serve frontend from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 /** @format */
 
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
@@ -10,8 +11,17 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
